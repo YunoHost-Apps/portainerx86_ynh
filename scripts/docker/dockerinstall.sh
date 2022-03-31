49e4feb4f58c95f524c0932a4a657b0e6ad37e84
jarod5001 <68397534+jarod5001@users.noreply.github.com>
2022-03-31 22:40:47 +0100

    Create dockerinstall.sh

---
+++ scripts/docker/dockerinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update
+sudo apt -y install docker-ce
+# docker-ce-cli containerd.io
+# sudo systemctl enable --now docker
+sudo usermod -aG docker admin
