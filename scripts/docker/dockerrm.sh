462fbbd16eee186845291801031c207cd75da87c
jarod5001 <68397534+jarod5001@users.noreply.github.com>
2022-04-02 00:23:44 +0200

    Update dockerrm.sh

--- scripts/docker/dockerrm.sh
+++ scripts/docker/dockerrm.sh
@@ -3,6 +3,14 @@
 # Remove Docker
 sudo apt-get purge docker-ce docker-ce-cli containerd.io -y
 
+# Undo add-apt-repository and apt-key
+sudo add-apt-repository -r \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key del -
+sudo apt update
+
 # Images, containers, volumes, or customized configuration files on your host are not automatically removed. To delete all images, containers, and volumes:
 # sudo rm -rf /var/lib/docker
 # sudo rm -rf /var/lib/containerd
