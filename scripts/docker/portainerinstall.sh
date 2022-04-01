22fd462ea1aaff5ce93af624819d8ee1004299b1
jarod5001 <68397534+jarod5001@users.noreply.github.com>
2022-04-01 21:20:16 +0200

    Update portainerinstall.sh

--- scripts/docker/portainerinstall.sh
+++ scripts/docker/portainerinstall.sh
@@ -3,6 +3,7 @@
 # Install portainer
 
 docker volume create portainer_data
+docker pull portainer/portainer-ce:2.11.1
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
     --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
