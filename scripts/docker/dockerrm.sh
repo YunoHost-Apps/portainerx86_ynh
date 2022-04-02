adb6879502a3304204ce486bca55312568324573
jarod5001 <68397534+jarod5001@users.noreply.github.com>
2022-04-02 02:29:26 +0200

    Update dockerrm.sh

--- scripts/docker/dockerrm.sh
+++ scripts/docker/dockerrm.sh
@@ -12,7 +12,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key del -
 sudo apt update
 
 # Remove other packs
-sudo apt remove -y dbus-user-session pigz
+sudo apt remove -y dbus-user-session pigz gnupg2 libglib2.0-data libpolkit-agent-1-0 libpolkit-backend-1-0 libpolkit-gobject-1-0 policykit-1
 
 # Images, containers, volumes, or customized configuration files on your host are not automatically removed. To delete all images, containers, and volumes:
 sudo rm -rf /var/lib/docker
