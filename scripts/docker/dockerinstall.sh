3c80e53e9deed426960f42e74cf923e8ab311a90
jarod5001 <68397534+jarod5001@users.noreply.github.com>
2022-04-02 05:33:37 +0100

    Update dockerinstall.sh

--- scripts/docker/dockerinstall.sh
+++ scripts/docker/dockerinstall.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 # Install docker
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
-sudo apt update 2>/dev/null
-sudo apt -y install docker-ce
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update 2>/dev/null
+sudo apt-get -y install docker-ce
+
 # sudo /usr/bin/dockerd -H unix://
 # docker-ce-cli containerd.io
 # sudo systemctl enable --now docker
+
+# Add user to docker group 
+sudo groupadd docker
 sudo usermod -aG docker admin
+sudo newgrp docker 
+
+# Logout needed here
