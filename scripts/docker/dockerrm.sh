8b0f2fc6096678e549456b2c5c399674d553a1d4
jarod5001 <68397534+jarod5001@users.noreply.github.com>
2022-04-02 03:57:40 +0200

    Update dockerrm.sh

--- scripts/docker/dockerrm.sh
+++ scripts/docker/dockerrm.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Remove Docker
-sudo apt-get purge docker-ce docker-ce-cli containerd.io -y
-
 # Undo add-apt-repository and apt-key
 sudo add-apt-repository -r \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
@@ -10,6 +7,9 @@ sudo add-apt-repository -r \
    stable"
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key del -
 sudo apt update
+
+# Remove Docker
+sudo apt-get purge docker-ce docker-ce-cli containerd.io -y
 
 # Remove other packs
 sudo apt remove -y dbus-user-session docker-scan-plugin pigz gnupg2 libglib2.0-data libpolkit-agent-1-0 libpolkit-backend-1-0 libpolkit-gobject-1-0 policykit-1
