d3de2412aefc1e0f537ec994bf5817f513bd4061
jarod5001 <68397534+jarod5001@users.noreply.github.com>
2022-04-03 13:25:39 +0200

    Update dockerrm.sh

--- scripts/docker/dockerrm.sh
+++ scripts/docker/dockerrm.sh
@@ -8,12 +8,17 @@ sudo apt-get update
 # Remove Docker
 sudo apt-get purge docker-ce docker-ce-cli containerd.io -y
 
+# Clean up
+rm /etc/docker/key.json
+rm -r /var/run/docker
+rm /var/run/docker.sock
+
 # Remove other packs
 # sudo apt remove -y dbus-user-session docker-scan-plugin pigz gnupg2 libglib2.0-data libpolkit-agent-1-0 libpolkit-backend-1-0 libpolkit-gobject-1-0 policykit-1
 # sudo apt autoremove -y
 
 # Remove admin from group docker and delete docker group
-# sudo deluser admin docker
+sudo deluser admin docker
 # sudo groupdel docker
 
 # Images, containers, volumes, or customized configuration files on your host are not automatically removed. To delete all images, containers, and volumes:
