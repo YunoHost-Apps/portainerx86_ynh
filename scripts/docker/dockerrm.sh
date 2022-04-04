2bee5f7f7518b5eb354baae4f66203123c745d25
jarod5001 <68397534+jarod5001@users.noreply.github.com>
2022-04-04 20:46:07 +0200

    Update dockerrm.sh

--- scripts/docker/dockerrm.sh
+++ scripts/docker/dockerrm.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
+# Remove Docker
+sudo apt-get purge docker-ce docker-ce-cli containerd.io -y
+
 # Undo add-apt-repository and apt-key
 sudo rm /usr/share/keyrings/docker-archive-keyring.gpg
 sudo rm /etc/apt/sources.list.d/docker.list 
 sudo apt-get update
 
-# Remove Docker
-sudo apt-get purge docker-ce docker-ce-cli containerd.io -y
-
 # Clean up
-rm /etc/docker/key.json
-rm -r /var/run/docker
-rm /var/run/docker.sock
-rm -r /var/run/containerd
+sudo rm /etc/docker/key.json
+sudo rm -r /var/run/docker
+sudo rm /var/run/docker.sock
+sudo rm -r /var/run/containerd
 
 # Remove other packs
 # sudo apt remove -y dbus-user-session docker-scan-plugin pigz gnupg2 libglib2.0-data libpolkit-agent-1-0 libpolkit-backend-1-0 libpolkit-gobject-1-0 policykit-1
